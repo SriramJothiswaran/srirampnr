c4d45e9a481071c31c3f06baf52dc988082017ee
Sriram Jothiswaran <j.sri.sriram@gmail.com>
2017-12-02 20:20:52 +0530

    dance

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const path = require('path');
+const request = require('request');
+var trainApiKey = "i0tm7cgqa6";
 
 
 var app = express();
@@ -14,9 +16,35 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//
+//app.get('/',function(req,res){
+//   res.render('search',{});
+//});
 
-app.get('/',function(req,res){
-   res.render('home',{});
+
+
+app.post('/trainstatus',function(req,res){
+   let trainno = req.body.trainno;
+   let today = new Date();
+
+   var dd = today.getDate();
+   var mm = today.getMonth()+1; //January is 0!
+
+   var yyyy = today.getFullYear();
+   if(dd<10){
+      dd='0'+dd;
+   }
+   if(mm<10){
+      mm='0'+mm;
+   }
+   today = dd+'-'+mm+'-'+yyyy;
+   let apikey = trainApiKey;
+   let url = `https://api.railwayapi.com/v2/live/train/${trainno}/date/${today}/apikey/${apikey}/`;
+   console.log(url);
+
+   request(url, function (err, response, body) {
+      console.log(response);
+   });
 });
 
 
