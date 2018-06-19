28be5b592d7436bb03969a3f9a0fefdc17650615
Sriram Jothiswaran <j.sri.sriram@gmail.com>
2018-06-20 02:38:42 +0530

    fetch data from ca.json and pouplate in dropdown

--- public/example/bluewasblack.py
+++ public/example/bluewasblack.py
@@ -1,13 +1,44 @@
 import tkinter as tk
+from  tkinter import END
+import json
+from pprint import pprint
 
 root = tk.Tk()
 root.title("City Information")
+variable = tk.StringVar(root)
+variable.set("Adelanto")
+initialName = "Adelanto"
+cityList = []
+with open('ca.json') as f:
+    data = json.load(f)
 
 
 
 
+for nameList in data:
+    cityList.append(nameList["name"])
+    if nameList["name"] == initialName:
+        countyValue = nameList["county_name"]
+        latitudeValue = nameList["primary_latitude"]
+        longitudeValue = nameList["primary_longitude"]
 
-N = 3
+cityList.sort()
+
+def OptionMenu_SelectionEvent(event):
+    for nameList in data:
+        cityList.append(nameList["name"])
+        if nameList["name"] == event:
+            entryboxes[frames[0]][0].delete(0,END)
+            entryboxes[frames[1]][0].delete(0,END)
+            entryboxes[frames[2]][0].delete(0,END)
+            countyValue = nameList["county_name"]
+            latitudeValue = nameList["primary_latitude"]
+            longitudeValue = nameList["primary_longitude"]
+            entryboxes[frames[0]][0].insert(0,countyValue)
+            entryboxes[frames[1]][0].insert(0, latitudeValue)
+            entryboxes[frames[2]][0].insert(0, longitudeValue)
+
+N = 4
 frames = []
 for n in range(N):
     frame = tk.Frame(root)
@@ -25,18 +56,23 @@ for i, frame in enumerate(frames):
     elif i == 2:
         label = tk.Label(frame, text="Longitude", anchor="w",width=10)
         label.pack(side='left')
-  
+    elif i == 3:
+        label = tk.Label(frame, text="City", anchor="w",width=10)
+        label.pack(side='left')
 
     for j in range(1):
         if i !=3:
             e = tk.Entry(frame, width = 50)
             e.pack(side='left')
             entryboxes[frame].append(e)
-     
+        else:
+            w = tk.OptionMenu(root, variable, *cityList, command = OptionMenu_SelectionEvent)
+            w.pack()
 
-entryboxes[frames[0]][0].insert(0, 'Los Angeles County')
-entryboxes[frames[1]][0].insert(0, 33.88)
-entryboxes[frames[2]][0].insert(0, -118.3)
+entryboxes[frames[0]][0].insert(0, countyValue)
+entryboxes[frames[1]][0].insert(0, latitudeValue)
+entryboxes[frames[2]][0].insert(0, longitudeValue)
+
 
 
 root.mainloop()
